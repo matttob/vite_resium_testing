c0db63a9469a9b00a9c411b9ca86429e149b3f8a
matttob <matttoberman@ghotmail.com>
2023-10-05 10:53:47 -0600

    removed hover test

--- src/App.jsx
+++ src/App.jsx
@@ -5,118 +5,29 @@ import { Cartesian3, Color, viewerCesiumInspectorMixin ,viewerCesium3DTilesInspe
 import { Viewer, Entity , GeoJsonDataSource, KmlDataSource,CameraFlyTo, Cesium3DTileset, ScreenSpaceEventHandler } from 'resium'
 
 
-
-
 import './App.css'
-
-
-// const position = Cartesian3.fromDegrees( -5.443517,  56.451267, 100);
-// const pointGraphics = { pixelSize: 10 };
-
-// const data = {
-//   type: "Feature",
-//   properties: {
-//     name: "ard_bay",
-//     amenity: "Ardmucknish Bay",
-//     popupContent: "This is where we work",
-//   },
-//   geometry: {
-//     type: "Point",
-//     coordinates: [-104.99404, 39.75621],
-//   },
-// };
-
-// const tileset = viewer.scene.primitives.add(
-//   await Cesium.Cesium3DTileset.fromIonAssetId(2293352)
-// );
-
-
-// function App() {
-  
-//   const viewRef = useRef(null);
-
-//   useEffect(() => {
-//     console.log('rendered')
-
-//     let handler;
-//     if (viewRef.current){
-//       console.log('making a listener')
-//       handler = new ScreenSpaceEventHandler(viewRef.current.cesiumElement.scene.canvas);
-//       handler.setInputAction(function(click) {
-//           const pickedObject = viewRef.current.cesiumElement.scene.pick(click.position);
-//           if (CesiumDefined(pickedObject) && pickedObject.id === 'uniqueID'){
-//               console.log(pickedObject.id)
-//           }
-//           console.log(pickedObject)
-//       }, ScreenSpaceEventType.LEFT_CLICK);
-
-//     }
-//     }, [
-//       //destroy the listener here
-//     ]); 
-
-//   function clickTest() {
-//     console.log("test")
-//   }
-
-//   return (
-//     <>
-//       <div className="cesium-container">
-//       <Viewer>
-//         <Entity position={position} point={pointGraphics} />
-//         <CameraFlyTo destination={Cartesian3.fromDegrees(-5.43545876445209,  56.45732764483844, 1000)} />
-//         <GeoJsonDataSource data={"./22_Alcyionium.json"} />
-
-//       </Viewer>
-    
-//       </div>
-
-
-//     </>
-
-      
-    
-    
-
-
-//   )
-// }
-
-// export default App
 
 
 
 function App() {
 
-
-  // let viewer; // This will be raw Cesium's Viewer object.
-
-  // const handleReady = tileset => {
-   
-  //     viewer.zoomTo(tileset);
-  //     console.log(tileset.show)
-  //     tileset.tileLoad.addEventListener(function(tile) {
-  //       console.log('A tile was loaded.');
-  //   });
-
-  // };
   Ion.defaultAccessToken ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzYjM5M2JiYy03ODhiLTQ2YmUtODhkNC0yNTdlZTQ2Y2RkOGMiLCJpZCI6MTU4OTgxLCJpYXQiOjE2OTY0MzgyNjJ9.4DRtmcWO-nxpnuMP8hNoq8AYgyy3ZQYYfxuZQ_p0W1w";
   
-
   let viewer;
   const handleReady = tileset => {
+    
     console.log(tileset.statistics)
+    console.log("test")
+    console.log(viewer)
+    viewer.extend(Cesium.viewerCesium3DTilesInspectorMixin);
     viewer.zoomTo(tileset)
     viewer.scene.requestRenderMode = true;
-    
+  
+   
   };
-
-
-
 
   return (
     <Viewer
-    baseLayerPicker={false}
       full
       ref={e => {
         viewer = e && e.cesiumElement;
@@ -129,7 +40,6 @@ function App() {
       debugShowContentBoundingVolume = {true}
 
       />
-      {/* <CameraFlyTo destination={Cartesian3.fromDegrees(-5.43545876445209,  56.45732764483844, 1000)} /> */}
     </Viewer>
 
   );
